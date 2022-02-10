5dbecd1bd038957341013bda86dc3d398c50b932
Mkheir13 <kheireddine.mederreg@ynov.com>
2022-02-10 15:28:09 +0100

    exo 1

--- exos/exo1.py
+++ exos/exo1.py
@@ -7,7 +7,7 @@ def exo_1():
     while nbr_siriacus != 1:
         if (nbr_siriacus % 2) == 0:
             nbr_siriacus = nbr_siriacus / 2
-            print(nbr_siriacus)
+            print(int(nbr_siriacus))
         else:
             nbr_siriacus = (nbr_siriacus * 3 + 1)
-            print(nbr_siriacus)
+            print(int(nbr_siriacus))
