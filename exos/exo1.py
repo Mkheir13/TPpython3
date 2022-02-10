342ddc212c0875d3ef16f4da08a969c1122299af
Bahsiik <o.mist1303@gmail.com>
2022-02-10 15:30:47 +0100

    ajout end au print exo 1

--- exos/exo1.py
+++ exos/exo1.py
@@ -7,7 +7,7 @@ def exo_1():
     while nbr_siriacus != 1:
         if (nbr_siriacus % 2) == 0:
             nbr_siriacus = nbr_siriacus / 2
-            print(int(nbr_siriacus))
+            print(int(nbr_siriacus), end=" ")
         else:
             nbr_siriacus = (nbr_siriacus * 3 + 1)
-            print(int(nbr_siriacus))
+            print(int(nbr_siriacus), end=" ")
