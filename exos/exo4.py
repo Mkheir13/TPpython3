d622241c3a696235bc47ca157d8260bfcb9b3d04
Bahsiik <o.mist1303@gmail.com>
2022-02-24 17:42:19 +0100

    debut exo4

--- exos/exo4.py
+++ exos/exo4.py
@@ -1,6 +1,25 @@
 from utilitary import *
 
 
+class Person:
+    def __init__(self, p_name, p_surname, p_address, p_nbr):
+        self.name = p_name
+        self.surname = p_surname
+        self.address = p_address
+        self.nbr = p_nbr
+
+
+
+    def display(self):
+        print("Nom : ", self.name)
+        print("Prénom : ", self.surname)
+        print("Adresse : ", self.address)
+        print("Numéro : ", self.nbr)
+
+
 # Algorithme définissant une suite mathématique
 def exo_4():
     print("\n\n\n\n======== Exercice 4 - Classe Personne ========\n")
+    p1 = Person("Eddine", "Kheir", "CACA", "055055")
+    p1.display()
+
